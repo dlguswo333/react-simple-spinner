b6f1e2f03a71a8d68920f9c115992ebfb4c385fd
dlguswo333 <dlguswo333@skku.edu>
2021-08-15 13:58:49 +0900

    Add Spinner stroke-width test

--- lib/Spinner.d.ts
+++ lib/Spinner.d.ts
@@ -1,6 +1,7 @@
 /// <reference types="react" />
 declare type Props = {
     size?: string;
+    width?: string;
     fill: boolean;
     colors: string[];
 };
@@ -8,5 +9,5 @@ declare type Props = {
  * @param {number} deg The degree value in 360°.
  */
 declare function getD(clockwise: boolean, fill: boolean, deg: number): string;
-declare function Spinner({ size, fill, colors }: Props): JSX.Element;
+declare function Spinner({ size, width, fill, colors }: Props): JSX.Element;
 export { Spinner as default, getD };
